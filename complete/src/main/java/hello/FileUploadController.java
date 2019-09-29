55249d5e66af18e1ae8b1e14449b940fc0d28dee
Jonathan-Can Kleiner <canozbek1@yahoo.com>
2019-09-29 22:56:19 +0200

    changes

--- complete/src/main/java/hello/FileUploadController.java
+++ complete/src/main/java/hello/FileUploadController.java
@@ -10,7 +10,8 @@ import java.nio.file.Paths;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.tomcat.util.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -18,34 +19,27 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import hello.storage.StorageFileNotFoundException;
-import hello.storage.StorageService;
 
 @Controller
 public class FileUploadController {
 
-	private final StorageService storageService;
-
 	private static final String EXTENSION = ".jpg";
 	private static final String SERVER_LOCATION = "upload-dir";
 
+	private static final Logger LOG = LoggerFactory.getLogger(FileUploadController.class);
+
 	@RequestMapping(path = "/download", method = RequestMethod.GET)
-	public ResponseEntity<Resource> download(@RequestParam("image") String image) throws IOException {
-		File file = new File(SERVER_LOCATION + File.separator + image + EXTENSION);
+	public ResponseEntity<Resource> download(@RequestParam("image") String imageName) throws IOException {
+		LOG.info("/download?image=" + imageName + " is called");
+		File file = new File(SERVER_LOCATION + File.separator + imageName + EXTENSION);
 
 		HttpHeaders header = new HttpHeaders();
-		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=img.jpg");
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=processedImage.jpg");
 		header.add("Cache-Control", "no-cache, no-store, must-revalidate");
 		header.add("Pragma", "no-cache");
 		header.add("Expires", "0");
@@ -62,6 +56,7 @@ public class FileUploadController {
 
 	@RequestMapping(path = "/apk", method = RequestMethod.GET)
 	public ResponseEntity<Resource> apk() throws IOException {
+		LOG.info("/apk is called");
 		File file = new File(SERVER_LOCATION + File.separator + "prototype.apk");
 
 		HttpHeaders header = new HttpHeaders();
@@ -80,11 +75,6 @@ public class FileUploadController {
 				.body(resource);
 	}
 
-	@Autowired
-	public FileUploadController(StorageService storageService) {
-		this.storageService = storageService;
-	}
-
 	@GetMapping("/")
 	public String listUploadedFiles(Model model) throws IOException {
 
@@ -93,30 +83,32 @@ public class FileUploadController {
 		//						"serveFile", path.getFileName().toString()).build().toString())
 		//				.collect(Collectors.toList()));
 
-		return "uploadForm";
+		LOG.info("homepage is called");
+
+		return "index";
 	}
 
-	@GetMapping("/files/{filename:.+}")
-	@ResponseBody
-	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+	//	@GetMapping("/files/{filename:.+}")
+	//	@ResponseBody
+	//	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+	//
+	//		Resource file = storageService.loadAsResource(filename);
+	//		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+	//				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+	//	}
 
-		Resource file = storageService.loadAsResource(filename);
-		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
-	}
-
-	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file,
-			RedirectAttributes redirectAttributes) {
-
-		System.out.println("--- UPLOADING A FILE ---");
-
-		storageService.store(file);
-		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded " + file.getOriginalFilename() + "!");
-
-		return "redirect:/";
-	}
+	//	@PostMapping("/")
+	//	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+	//			RedirectAttributes redirectAttributes) {
+	//
+	//		LOG.info("Uploading a file ");
+	//
+	//		storageService.store(file);
+	//		redirectAttributes.addFlashAttribute("message",
+	//				"You successfully uploaded " + file.getOriginalFilename() + "!");
+	//
+	//		return "redirect:/";
+	//	}
 
 	@RequestMapping(value = "/magic", method = RequestMethod.POST)
 	public @ResponseBody String uploadImage2(@RequestParam("image_key") String imageValue,
@@ -124,11 +116,10 @@ public class FileUploadController {
 			@RequestParam("name_key") String imageName) {
 		try {
 
-			System.out.println(" *-*-*-*-*   MAGIC IS HAPPENING -*-*-*-*-");
+			LOG.info("/magic is called - starting image processing...");
 
 			imageName = "blabla.jpeg";
 
-			System.out.println("Name: " + imageName);
 			//This will decode the String which is encoded by using Base64 class
 			byte[] imageByte = Base64.decodeBase64(imageValue);
 
@@ -141,35 +132,23 @@ public class FileUploadController {
 			// process image 
 
 			//			Process p = Runtime.getRuntime().exec("python yourapp.py");
-
 			File workingDirectory = new File("upload-dir");
 
 			ProcessBuilder processBuilder = new ProcessBuilder().directory(workingDirectory);
-			//			processBuilder.command("cmd.exe", "/c", "python process.py " + imageName);
-			processBuilder.command("/bin/bash", "-c", "python process.py " + imageName);
+			processBuilder.command("cmd.exe", "/c", "python process.py " + imageName);
+			//			processBuilder.command("/bin/bash", "-c", "python process.py " + imageName);
 			Process process = processBuilder.start();
 			int exitCode = process.waitFor(); // returns 0 on success, 1 on failure
 
-			System.out.println("Exit code: " + exitCode);
+			LOG.info("python script returned with exit code: " + exitCode);
 
 			return "success ";
 		}
 		catch (Exception e) {
+			LOG.info(e.toString());
 			return "error = " + e;
 		}
 
-	}
-
-	//	@GetMapping("processedimage")
-	//	public File test() {
-	//
-	//		return null;
-	//
-	//	}
-
-	@ExceptionHandler(StorageFileNotFoundException.class)
-	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-		return ResponseEntity.notFound().build();
 	}
 
 }
